53d7722a41d0c8ede708f30dd545b05a8b8964ac
2239559319 <w2239559319@outlook.com>
2024-05-02 14:13:42 +0800

    fix react esm

---
+++ esm/react/18.2.0/react-dom.mjs
@@ -0,0 +1,4 @@
+import * as ReactDOM from './react-dom.esm.js';
+
+export * from './react-dom.esm.js';
+export default ReactDOM;
---
+++ esm/react/18.2.0/scheduler.mjs
@@ -0,0 +1,4 @@
+import * as scheduler from './scheduler.esm.js';
+
+export * from './scheduler.esm.js';
+export default scheduler;
