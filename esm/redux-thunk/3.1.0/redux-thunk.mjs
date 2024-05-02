7ca143398cdd11031a56365740e437e66e31e96e
2239559319 <w2239559319@outlook.com>
2024-05-02 22:22:17 +0800

    redux thunk

---
+++ esm/redux-thunk/3.1.0/redux-thunk.mjs
@@ -0,0 +1,17 @@
+// src/index.ts
+function createThunkMiddleware(extraArgument) {
+  const middleware = ({ dispatch, getState }) => (next) => (action) => {
+    if (typeof action === "function") {
+      return action(dispatch, getState, extraArgument);
+    }
+    return next(action);
+  };
+  return middleware;
+}
+var thunk = createThunkMiddleware();
+var withExtraArgument = createThunkMiddleware;
+export {
+  thunk,
+  withExtraArgument
+};
+//# sourceMappingURL=redux-thunk.mjs.map
